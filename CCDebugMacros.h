0ed32c48787e26957e7f3961271d8301f57da1cb
Martin Gratzer <mgratzer@gmail.com>
2010-12-18 10:29:03 +0100

    initial XCode project setup

---
+++ CCDebugMacros.h
@@ -0,0 +1,17 @@
+//
+//  CCDebugMacros.h
+//  CleanCity
+//
+//  Created by Martin Gratzer on 18.12.10.
+//  Copyright 2010 CodeCamp Klagenfurt. All rights reserved.
+//
+
+#if defined(APP_STORE_FINAL)
+	#define CCLOG(format, ...)
+#else
+	#define CCLOG(format, ...) CFShow([NSString stringWithFormat: format, \
+	## __VA_ARGS__]);
+#endif
+
+#define CCLOGERR(format, ...) { NSLog((@"%s [Line %d] " format), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__); [MMErrorLog logErrorInFunction:(char*)__PRETTY_FUNCTION__ atLine:__LINE__ withFormat:(format),##__VA_ARGS__]; }
+
---
+++ iPad/AppDelegate_iPad.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate_iPad.h
+//  CleanCity
+//
+//  Created by Martin Gratzer on 18.12.10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate_iPad : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
